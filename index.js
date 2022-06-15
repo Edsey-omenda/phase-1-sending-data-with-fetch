8df3a15e9ae45fba915cb0c8bd8e22942384dccb
Edsey omenda <omendajames0317@gmail.com>
2022-06-15 18:41:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+/*const userData = {
+    userName: "James",
+    userEmail: "omendajames0317@gmail.com",
+  };*/
+
+/*const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const user_name = document.querySelector('#user_name').value;
+    const user_email = document.querySelector('#user_eail').value;
+    submitData(user_name, user_email);
+
+})*/
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+};
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        let details = document.createElement('p')
+        details.textContent = `${data.id}`
+        document.querySelector('body').appendChild(details)
+       
+    })
+    .catch(function (error){
+        console.log(error.message)
+        let message = document.createElement('p')
+        message.textContent = `
+        message: 'Unauthorized Access'
+        `
+        document.querySelector('body').appendChild(message)
+    })
+};
+submitData();
